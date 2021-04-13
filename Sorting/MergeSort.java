081ae75dd0a2bd8a13ca1ba8666376c0c30f2d1b
linjiang82 <linjiang82@gmail.com>
2021-04-13 23:04:38 +1000

    modified merge method

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,5 +1,9 @@
 package Sorting;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MergeSort {
     public static int[] sort(int[] arr){
         int len= arr.length;
@@ -11,7 +15,7 @@ public class MergeSort {
             int[] right;
                     middle = Math.floorDiv(len,2);
                     left=copy(0,middle,arr);
-            right=copy(0,middle,arr);
+            right=copy(middle,len-1,arr);
             return merge(sort(left),sort(right));
         }
     }
@@ -25,13 +29,25 @@ public class MergeSort {
     }
     private static int[] merge(int[] left, int[] right){
         int[] resultArr;
-        if(left.length==0)
-            return right;
-        if(right.length==0)
-            return left;
-       if(left[0]<right[0]){
-           resultArr[idx++]=left[0];
-       }
-       else resultArr[]
+        int idx;
+        List<Integer> leftList = Arrays.stream(left).boxed().collect(Collectors.toList());
+      List<Integer>  rightList = Arrays.stream(right).boxed().collect(Collectors.toList());
+        if(leftList.size()==0) {
+            return rightList.toArray(new int[]);
+        }
+      if(rightList.size()==0) {
+          return leftList.toArray(new int[]);
+      }
+      if(leftList.get(0)>rightList.get(0)){
+         resultArr[idx]=rightList.get(0);
+         rightList.remove(0);
+      }
+      else {
+
+          resultArr[idx]=leftList.get(0);
+          leftList.remove(0);
+      }
+
+      return resultArr;
     }
 }
